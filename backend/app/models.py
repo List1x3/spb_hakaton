a479a70fcd0215029ea17f993cf4cd8daf81d8ef
root <root@slanalan12031.serv.host>
2025-11-29 16:52:29 +0000

    Добавил поля для курсов

--- backend/app/models.py
+++ backend/app/models.py
@@ -8,7 +8,20 @@ class User(db.Model):
     username = db.Column(db.String(120), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
-    avatar = db.Column(db.String(255), nullable=False, default="default_avatar.jpg")
+    avatar = db.Column(db.String(255), nullable=False, default="Unknown_person.jpg")
 
     def __repr__(self):
         return f"<User {self.username}>"
+
+
+class Course(db.Model):
+    __tablename__ = "courses"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    author = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    url = db.Column(db.String(500), nullable=True)
+
+    def __repr__(self):
+        return f"<Course {self.name}>"
